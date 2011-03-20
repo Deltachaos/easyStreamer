a2160b3842a1727ade92c9f89a915e58148f17fb
deltachaos <redplug@web.de>
2011-03-20 07:53:16 +0100

    fixing copyright

--- daemon.php
+++ daemon.php
@@ -4,7 +4,7 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 20011, Maximilian Ruta
+ * @copyright     Copyright 2011, Maximilian Ruta
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 define('PATH', dirname(__FILE__));
